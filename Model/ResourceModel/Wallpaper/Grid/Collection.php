c3bfd8e97692fdb564362d23f08e448554e8e768
ngoducnhuiit <ngoducnhuiit@gmail.com>
2020-12-21 19:58:25 +0700

    Update di.xml

---
+++ Model/ResourceModel/Wallpaper/Grid/Collection.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Collection
+ *
+ * @copyright Copyright © 2020 Magepow. All rights reserved.
+ * @author    Magepow
+ * @category Magepow
+ * @copyright Copyright (c) 2014 Magepow (<https://www.magepow.com>)
+ * @license <https://www.magepow.com/license-agreement.html>
+ * @Author: magepow<support@magepow.com>
+ * @github: <https://github.com/magepow>
+ */
+
+namespace Magepow\Wallpaper\Model\ResourceModel\Wallpaper\Grid;
+
+use Magento\Framework\Api\Search\SearchResultInterface;
+use Magento\Framework\Search\AggregationInterface;
+use Magepow\Wallpaper\Model\ResourceModel\Wallpaper\Collection as QuoteCollection;
+class Collection extends QuoteCollection implements SearchResultInterface
+{
+    protected $aggregations;
+    public function __construct(
+        \Magento\Framework\Data\Collection\EntityFactoryInterface $entityFactory,
+        \Psr\Log\LoggerInterface $logger,
+        \Magento\Framework\Data\Collection\Db\FetchStrategyInterface $fetchStrategy,
+        \Magento\Framework\Event\ManagerInterface $eventManager,
+        $mainTable,
+        $eventPrefix,
+        $eventObject,
+        $resourceModel,
+        $model = 'Magento\Framework\View\Element\UiComponent\DataProvider\Document',
+        $connection = null,
+        \Magento\Framework\Model\ResourceModel\Db\AbstractDb $resource = null
+    ) {
+        parent::__construct(
+            $entityFactory,
+            $logger,
+            $fetchStrategy,
+            $eventManager,
+            $connection,
+            $resource
+        );
+        $this->_eventPrefix = $eventPrefix;
+        $this->_eventObject = $eventObject;
+        $this->_init($model, $resourceModel);
+        $this->setMainTable($mainTable);
+    }
+
+    public function getAggregations()
+    {
+        return $this->aggregations;
+    }
+
+    public function setAggregations($aggregations)
+    {
+        $this->aggregations = $aggregations;
+    }
+
+    public function getAllIds($limit = null, $offset = null)
+    {
+        return $this->getConnection()->fetchCol($this->_getAllIdsSelect($limit, $offset), $this->_bindParams);
+    }
+
+    public function getSearchCriteria()
+    {
+        return null;
+    }
+
+    public function setSearchCriteria(\Magento\Framework\Api\SearchCriteriaInterface $searchCriteria = null)
+    {
+        return $this;
+    }
+
+    public function getTotalCount()
+    {
+        return $this->getSize();
+    }
+
+    public function setTotalCount($totalCount)
+    {
+        return $this;
+    }
+    public function setItems(array $items = null)
+    {
+        return $this;
+    }
+}
